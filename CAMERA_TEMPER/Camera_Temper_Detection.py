ec066fcf09fab9644ed305b6b65c0bbe0ca303dc
Siddhant Rai <35813591+siddhant230@users.noreply.github.com>
2020-01-17 15:20:21 +0530

    Update Camera_Temper_Detection.py

--- CAMERA_TEMPER/Camera_Temper_Detection.py
+++ CAMERA_TEMPER/Camera_Temper_Detection.py
@@ -3,9 +3,7 @@ import numpy as np
 import time
 import smtplib
 
-path='/home/parmeet/Desktop/scrofppt/opencvvid/lol.mkv' ##or another USB camera if you have any
 cap0=cv2.VideoCapture(0)
-cap1=cv2.VideoCapture(path)
 
 def check_all_pixels(img,factor):
     count=0
@@ -57,24 +55,16 @@ sent1=False
 while True:
     i+=1
     _,img0=cap0.read()
-    _,img1=cap1.read()
-    imgarr=[img0,img1]
-    print(cap1.read()[1])
+    imgarr=[img0]
 
     copy0=img0.copy()
-    copy1=img1.copy()
 
     (w0,h0,d0)=img0.shape
-    (w1,h1,d1)=img1.shape
 
     count0=check_all_pixels(img0,h0*d0)
     result0,lkv0=blur_detection(copy0)
     moved0=check_movement(img0,i,h0*d0)
-    count1=check_all_pixels(img1,h0*d0)
-    result1,lkv1=blur_detection(copy1)
-    moved1=check_movement(img1,i,h1*d1)
 
-    print(moved0,moved1)
     if (count0/w0)*100 >70:
         if counting0==False:
             counting0=True
@@ -85,7 +75,7 @@ while True:
             sent0=True
             s = smtplib.SMTP('smtp.gmail.com', 587)
             s.starttls()
-            s.login("rsiddhant73@gmail.com", "#pokemon911")
+            s.login("rsiddhant73@gmail.com", "************")
             message = "Your camera number 1 has been covered for more than 5 seconds"
             s.sendmail("rsiddhant73@gmail.com", "sidlovesml@gmail.com", message)
             s.quit()
@@ -96,42 +86,14 @@ while True:
         sent0=False
         counting0=False
 
-    if (count1/w1)*100 >70:
-        if counting1==False:
-            counting1=True
-            start=time.time()
-        text1='Camera covered'
-        duration1=time.time()-start
-        if duration1>5 and sent1==False:
-            sent1=True
-            s = smtplib.SMTP('smtp.gmail.com', 587)
-            s.starttls()
-            s.login("rsiddhant73@gmail.com", "your password")
-            message = "Your camera number 2 has been covered for more than 5 seconds"
-            s.sendmail("rsiddhant73@gmail.com", "sidlovesml@gmail.com", message)
-            s.quit()
-            print('sent email')
-
-    else:
-        text1=''
-        sent1=False
-        counting1=False
-
 
     cv2.putText(img0,text0,(10,40),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,2,246),1)
     cv2.putText(img0,result0+' : '+str(lkv0),(10,60),cv2.FONT_HERSHEY_COMPLEX,0.7,(100,2,246),1)
-    if lkv0<40:
+    if lkv0<125:
         cv2.putText(img0,'TOO MUCH BLURRY',(10,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(100,2,246),1)
     if moved0>60:
         cv2.putText(img0,'MOVED',(10,130),cv2.FONT_HERSHEY_SIMPLEX,0.7,(100,2,246),1)
     cv2.imshow('black',img0)
-    cv2.putText(img1,text1,(10,40),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,2,246),1)
-    cv2.putText(img1,result1+' : '+str(lkv0),(10,60),cv2.FONT_HERSHEY_COMPLEX,0.7,(100,2,246),1)
-    if lkv0<40:
-        cv2.putText(img1,'TOO MUCH BLURRY',(10,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(100,2,246),1)
-    if moved0>60:
-        cv2.putText(img1,'MOVED',(10,130),cv2.FONT_HERSHEY_SIMPLEX,0.7,(100,2,246),1)
-    cv2.imshow('black',img0)
-    cv2.imshow('black2',img1)
+
     if cv2.waitKey(1)==ord('q'):
         break
